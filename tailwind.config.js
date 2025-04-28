db3931449035412c9138b7f33ca9806cb6484e56
realUmasorDavid <145034172+realUmasorDavid@users.noreply.github.com>
2025-04-28 11:59:23 +0100

    Update v0.1b.2
    Updated Fonts

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+      './**/*.{html,js,ts,jsx,tsx,php}', // Scan all subdirectories
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          "Poppins": ['Poppins', 'serif'],
+        }
+      },
+    },
+    plugins: [],
+  };
